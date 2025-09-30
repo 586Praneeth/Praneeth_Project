ea2795a2237b538668b7af135c11c01c42ac17d5
Praneeth340 <14cn041@gmail.com>
2025-09-30 07:49:49 -0500

    Initial portfolio commit with Vite setup

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Praneeth_Nagilla_Portfolio/', // 👈 THIS LINE IS CRUCIAL
+  base: '/Praneeth_Project//', // 👈 THIS LINE IS CRUCIAL
   plugins: [react()],
 })
